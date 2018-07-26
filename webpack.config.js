d95f39bf3f9c63a8ae75ba1e73f31841541e8434
Rafael Henrique Berro <rhberro@gmail.com>
2018-07-25 23:34:42 -0300

    adding support to svg files

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ module.exports = {
       {
         test: /\.styl$/,
         loader: 'style-loader!css-loader!stylus-loader'
+      },
+      {
+        test: /\.svg$/,
+        loader: 'raw-loader'
       }
     ]
   },
