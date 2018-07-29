b776293b4ac41434aee4e926718f0c5dd249b9cd
Rafael Henrique Berro <rhberro@gmail.com>
2018-07-29 14:19:04 -0300

    updating the styles

--- source/index.js
+++ source/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom'
 
 import Layout from './views/Layout'
 
+import Footer from './components/Footer'
 import Header from './components/Header'
+import Playlists from './components/Playlists'
 
 ReactDOM.render(
   <Layout>
     <Header />
+    <Playlists />
+    <Footer />
   </Layout>,
   document.getElementById('application')
 )
