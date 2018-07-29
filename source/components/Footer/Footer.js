4dce408e1c959ef22c92a640fe7743c0a763a5b5
Rafael Henrique Berro <rhberro@gmail.com>
2018-07-29 14:14:35 -0300

    creating the footer component

---
+++ source/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Footer.styl'
+
+function Footer () {
+  return (
+    <footer className='footer'>
+      <div className='footer__container'>
+        <p className='footer__text'>© Spotifood, 2018.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
