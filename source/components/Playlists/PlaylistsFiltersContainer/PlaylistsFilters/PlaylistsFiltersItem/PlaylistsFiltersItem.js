9477e37d08fde7eb290d75e50a62a81e6b00561a
Rafael Henrique Berro <rhberro@gmail.com>
2018-07-30 03:10:22 -0300

    creating a new component that represents a filter

---
+++ source/components/Playlists/PlaylistsFiltersContainer/PlaylistsFilters/PlaylistsFiltersItem/PlaylistsFiltersItem.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+function renderOption (option) {
+  const { value, name } = option
+  return (
+    <option value={value} key={value}>
+      { name }
+    </option>
+  )
+}
+
+function renderSelect (item, props) {
+  const { id, name, values } = item
+  const { onChange, fields } = props
+  const value = fields[id]
+
+  return (
+    <div className='playlists-filter' key={id}>
+      <select className='playlists-filter__input' name={id} placeholder={name} value={value || ''} onChange={onChange}>
+        <option value=''>{ name }</option>
+        { values.map(renderOption) }
+      </select>
+    </div>
+  )
+}
+
+function renderText (item, props) {
+  const { id, name } = item
+  const { onChange, fields } = props
+  const value = fields[id]
+
+  return (
+    <div className='playlists-filter' key={id}>
+      <input className='playlists-filter__input' type='text' name={id} placeholder={name} value={value || ''} onChange={onChange} />
+    </div>
+  )
+}
+
+function PlaylistsFilterItem (props) {
+  const { values } = props
+
+  return values
+    ? renderSelect(props, this)
+    : renderText(props, this)
+}
+
+export default PlaylistsFilterItem
